308ee0f74e46aa2aca4df0bde23128603eb104a7
Levi <levi.meston360@gmail.com>
2020-10-30 21:50:42 -0600

    Base setup for spigot plugin.

---
+++ src/main/java/com/meevsbox/manHunt/manHunt.java
@@ -0,0 +1,23 @@
+/*
+ * Author -- Meevs
+ * Creation Date -- Fri Oct 30 21:43:30 2020
+ * File Name -- manHunt.java
+ * Notes -- 
+ */
+
+package com.meevsbox.manHunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class manHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable () {
+
+    }
+
+    @Override
+    public void onDisable () {
+
+    }
+}
